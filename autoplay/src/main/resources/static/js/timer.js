dbb8a111d1db6be793fdd9053fbc02300290dfe9
wwwalohacampus <h850415@naver.com>
2025-04-16 00:52:27 +0900

    TODO: 실제 설문조사 URL 로 변경하기

--- autoplay/src/main/resources/static/js/timer.js
+++ autoplay/src/main/resources/static/js/timer.js
@@ -6,7 +6,14 @@ $(function() {
       data: {}
     });
     if (response === true) {
-      $alert('알림', '접속 후 10분이 지났습니다. 설문조사에 응해주세요', 'info');
+      // $alert('알림', '접속 후 10분이 지났습니다. 설문조사에 응해주세요', 'info');
+      $confirm('설문조사 알림', '접속 후 10분이 지났습니다. 설문조사에 응해주세요', 'info', '설문조사 하러가기', '')
+        .then((result) => {
+          if (result.isConfirmed) {
+            // TODO: 설문조사 URL 변경
+            window.open('/survey', '_blank');
+          }
+        });
     }
   }, 20 * 1000);
   // 20초마다 서버에 요청을 보내고, 응답이 true일 경우 알림을 띄운다.
